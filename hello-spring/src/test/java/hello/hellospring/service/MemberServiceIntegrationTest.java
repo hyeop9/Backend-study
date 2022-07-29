0ab8de74ebe5a060e0bfd4b55a24ffd374103439
Hyunyeop Ahn <hyeop9@gmail.com>
2022-07-29 09:36:50 +0900

    Backend study

--- hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
+++ hello-spring/src/test/java/hello/hellospring/service/MemberServiceIntegrationTest.java
@@ -12,7 +12,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-
+// test git
 @SpringBootTest
 @Transactional
 class MemberServiceIntegrationTest {
