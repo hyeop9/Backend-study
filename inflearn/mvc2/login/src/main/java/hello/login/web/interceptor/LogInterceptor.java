c3fcc6b256c9271592c0d38b304de31b74f2db40
Hyunyeop Ahn <hyeop9@gmail.com>
2022-08-21 16:40:38 +0900

    spring

--- inflearn/mvc2/login/src/main/java/hello/login/web/interceptor/LogInterceptor.java
+++ inflearn/mvc2/login/src/main/java/hello/login/web/interceptor/LogInterceptor.java
@@ -1,6 +1,7 @@
 package hello.login.web.interceptor;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -20,15 +21,30 @@ public class LogInterceptor implements HandlerInterceptor {
         String uuid = UUID.randomUUID().toString();
 
         request.setAttribute(LOG_ID, uuid);
+
+        // @RequestMapping: HandlerMethod
+        // 정적 리소스: ResourceHttpRequestHandler
+
+        if (handler instanceof HandlerMethod) {
+            HandlerMethod hm = (HandlerMethod) handler; // 호출할 컨트롤러 메서드의 모든 정보가 포함되어 있다.
+        }
+
+        log.info("REQUEST [{}] [{}] [{}]", uuid, requestURI, handler);
+        return true;
     }
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-
+        log.info("postHandle [{}]", modelAndView);
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-        Object logId = request.getAttribute("LOG_ID");
+        String requestURI = request.getRequestURI();
+        String logId = (String) request.getAttribute("LOG_ID");
+        log.info("RESPONSE [{}] [{}] [{}]", LOG_ID, requestURI, handler);
+        if (ex != null) {
+            log.error("afterCompletion error!!", ex);
+        }
     }
 }
